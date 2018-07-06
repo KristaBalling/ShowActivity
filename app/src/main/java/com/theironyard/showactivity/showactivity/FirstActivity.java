7ece33e04352eebe40b6bfc4085e23777fbcf510
Krista Blachian <KristaBlachian@outlook.com>
2018-07-06 19:13:39 -0400

    initial commit

---
+++ app/src/main/java/com/theironyard/showactivity/showactivity/FirstActivity.java
@@ -0,0 +1,30 @@
+package com.theironyard.showactivity.showactivity;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class FirstActivity extends AppCompatActivity {
+    private Button goToSecondButton;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first);
+
+        goToSecondButton = (Button) findViewById(R.id.showButtonID);
+        goToSecondButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(FirstActivity.this, SecondActivity.class);
+                startActivity(intent);
+
+                //OR startActivity(new Intent(FirstActivity.this, SecondActivity.class));
+            }
+        });
+    }
+}
